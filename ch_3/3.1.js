9fefca7e07a0f071bf2b2de1fd9777ae09c8bb98
Rainier F. Go <rainier.f.go@gmail.com>
2016-12-02 23:57:23 -0500

    Ch 3.1 - Made a Javascript version of the solution

--- ch_3/3.1.js
+++ ch_3/3.1.js
@@ -1,4 +1,5 @@
 //STACKS & QUEUES
+/*SOLUTION USING JAVA
 int stackSize = 100; 
 int[] array = new int [stackSize * 3]; 
 int[] positionOfTopFor = {-1, -1, -1};
@@ -36,4 +37,75 @@ int peek(int stackNum) {
 boolean isEmpty(int stackNum) {
 	return accessTopOf[stackNum] == -1; 
 }
+*/
 
+//JAVASCRIPT VERSION
+function Stack(size) {
+	this.stackSize = size; 
+	this.array = [];
+	this.positionOfTopFor = [-1, -1, -1]; 
+}
+//FUNCTION TO CALCULATE THE POSITION OF THE TOP OF THE STACK
+//input: which stack to access
+//output: position to calculate the new index of top 
+Stack.prototype.getIndexOfTopFor = function(stackNum) {
+	//Not adding one after call to psoitionOfTopFor is based
+	//on my addition to increment the top before calling 
+	//changeIndexOfTopFor()
+	//Look at push function for more details
+	return stackNum * this.stackSize + this.positionOfTopFor[stackNum]; 
+}
+//FUNCTION TO ADD A VALUE TO THE RIGHT STACK 
+//input: which stack to access and value
+//output: none
+//exceptions: is stack full?
+Stack.prototype.push = function (value, stackNum) {
+	if(this.getIndexOfTopFor[stackNum] >= -1) 
+		return console.log("Out of space.");
+		
+	this.positionOfTopFor[stackNum]++; 
+	this.array[this.getIndexOfTopFor(stackNum)] = value; 
+	return;
+	
+}
+
+//FUNCTION TO REMOVE THE TOP OF THE STACK 
+//input: which stack to access
+//exceptions: is stack empty?
+Stack.prototype.pop = function(stackNum) {
+	if(this.positionOfTopFor[stackNum] == -1) 
+		return console.log("Empty stack.");
+	
+	var value = this.array[this.getIndexOfTopFor(stackNum)] 
+	this.array[this.getIndexOfTopFor(stackNum)] = 0; 
+	this.positionOfTopFor[stackNum]--;
+	
+	return;
+}
+
+//FUNCTION TO SEE IF SPECIFIC STACK IS EMPTY 
+//input: which stack to access
+Stack.prototype.isEmpty = function (stackNum) {
+	return this.positionOfTopFor[stackNum] == -1;
+}
+//FUNCTION TO RETURN THE TOP OF A CERTAIN STACK 
+//exceptions: is stack empty? 
+Stack.prototype.topOf = function (stackNum) {
+	var index = this.getIndexOfTopFor(stackNum);
+	return this.array[index];
+}
+
+var hello = new Stack(100);
+console.log(hello);
+hello.push(3, 0);
+hello.push(3, 0);
+hello.push(3, 0);
+hello.pop(0);
+hello.pop(1);
+console.log(hello.topOf(0));
+console.log(hello);
+console.log(hello.isEmpty(0));
+console.log(hello.isEmpty(1));
+console.log(hello.isEmpty(2));
+hello.push(3, 0);
+console.log(hello);
