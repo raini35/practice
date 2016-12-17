2d835b5aa21d64bfd2a5001257981b29b01c22ac
Rainier F. Go <rainier.f.go@gmail.com>
2016-12-16 23:15:24 -0500

    Started on 3.7.js

---
+++ ch_3/3.7.js
@@ -0,0 +1,16 @@
+function PetStore() {
+	this.inventory = []; 
+	this.capacity = 0; 
+}
+
+PetStore.prototype.add = function() {
+	//readline get type 
+	//readline get name 
+	//store type and name into inventory 
+}
+
+PetStore.prototype.adopt = function() {
+	//readline get type 
+	//readline get name 
+	//check if certain pet exists 
+}
